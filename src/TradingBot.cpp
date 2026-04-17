fa9ee458d63d21617ede5f9e124ac82916cbc449
Guy <gai.vadai@gmail.com>
2026-04-18 01:58:08 +0300

    init technically also folder plan

---
+++ src/TradingBot.cpp
@@ -0,0 +1,14 @@
+﻿// TradingBot.cpp : Defines the entry point for the application.
+//
+
+#include "TradingBot.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Begin Trading Bot." << endl;
+	return 0;
+}
+
+
