2d9b148e744920e603ce611f9bfccbbe99a94229
Guy <gai.vadai@gmail.com>
2026-04-24 15:33:27 +0300

    fixed rss

--- src/services/collectors/news/news.h
+++ src/services/collectors/news/news.h
@@ -1,3 +1,10 @@
+#include <string>
+#include <vector>
+#include <cpr/response.h>
+
 namespace news {
     struct Article;
+
+    cpr::Response get_ticker_headlines(const std::vector<std::string>& symbols);
+    cpr::Response rss_yahoo_news();
 }
