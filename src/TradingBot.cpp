e05e4559f17e16e2f399e52c546063e8cfb27255
Guy <gai.vadai@gmail.com>
2026-04-29 22:33:59 +0300

    rss refresh rates <ttl> and general parsing progress

--- src/TradingBot.cpp
+++ src/TradingBot.cpp
@@ -11,49 +11,22 @@ using namespace std;
 
 void run_all_test();
 void run_collectors_tests();
-void run_services_tests();
+void run_rss_tests();
 
 int main() {
     cout << "Begin Trading Bot." << endl;
 
-    // run_collectors_tests();
-    run_services_tests();
-    // run_utils_tests();
+    run_rss_tests();
+    // run_services_tests();
     cin.get();
     return 0;
 }
 
-void run_all_test() {
-    run_collectors_tests();
-}
+void run_all_test() { run_collectors_tests(); }
 
-// services tests
-void run_services_tests() {
+void run_collectors_tests() {
     cout << "--------services test--------" << endl << endl;
     time_t p1 = 1753035600;
-    time_t p2 = 1773039200;
-
-    api::Yahoo yahoo;
-    const auto& hist = yahoo.get_hist("PLTR", p1, p2, api::_1d);
-    const auto& quote = yahoo.get_quote({"PLTR", "AAPL", "GOOGL"});
-    const auto& ticker_news = yahoo.get_ticker_news({"PLTR", "AAPL", "GOOGL"}, 10);
-    const auto& headlines = news::yahoo::get_ticker_headlines({"PLTR", "AAPL", "GOOGL"});
-    const auto& rss_news = news::yahoo::rss_market_news();
-
-    json_utils::write_cache("data/history.json", json::parse(hist.text));
-    json_utils::write_cache("data/quote.json", json::parse(quote.text));
-    json_utils::write_cache("data/ticker_news.json", json::parse(ticker_news.text));
-    cout << "writing data/headlines.xml to cache" << endl;
-    xml_utils::parse(headlines.text).save_file("data/headlines.xml");
-    cout << "writing data/rss_news.xml to cache" << endl;
-    xml_utils::parse(rss_news.text).save_file("data/rss_news.xml");
-}
-
-// collectors tests
-void run_collectors_tests() {
-
-    cout << "--------collectors test--------" << endl << endl;
-    time_t p1 = 1763035600;
     time_t p2 = 1773039200;
 
     api::Yahoo yahoo;
@@ -69,5 +42,20 @@ void run_collectors_tests() {
     cout << "headlines test: " << headlines.status_line << endl;
     cout << "rss_news test: " << rss_news.status_line << endl;
 
+    json_utils::write_cache("data/history.json", json::parse(hist.text));
+    json_utils::write_cache("data/quote.json", json::parse(quote.text));
+    json_utils::write_cache("data/ticker_news.json", json::parse(ticker_news.text));
+    cout << "writing data/headlines.xml to cache" << endl;
+    xml_utils::parse(headlines.text).save_file("data/headlines.xml");
+    cout << "writing data/rss_news.xml to cache" << endl;
+    xml_utils::parse(rss_news.text).save_file("data/rss_news.xml");
+
     cout << "----test finished----" << endl << endl;
+}
+
+void run_rss_tests() {
+    news::Rss rss_manager = news::Rss();
+    rss_manager.subscribe(
+        {"https://feeds.finance.yahoo.com/rss/2.0/headline?s=yhoo,goog&region=US&lang=en-US", "https://news.yahoo.com/rss/"});
+    rss_manager.print_refresh_rates();
 }
--- src/services/collectors/news/news.h
+++ src/services/collectors/news/news.h
@@ -1,7 +1,9 @@
 #pragma once
+#include "cpr/cprtypes.h"
 #include "cpr/session.h"
 #include <cpr/response.h>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace news {
@@ -20,16 +22,29 @@ namespace news {
             /// add rss links to subscriptions
             void subscribe(const std::vector<std::string>& rss_links);
 
+            /// prints out current rss subscription refresh rates
+            void print_refresh_rates();
+
         private:
-            int refresh_rate;
             cpr::Session session;
             std::vector<cpr::Url> rss_links;
-            /// get all subscription responses at once
-            std::vector<cpr::Response> get_all_rss();
+            std::unordered_map<std::string, int> refresh_rates;
+
+            /// gets rss recommended refresh rate in minutes
+            /// <ttl> tag if exists for every subscriptions. otherwise sets to a default
+            void set_refresh_rates();
+
+            /// get specific subscription rss
+            cpr::Response get_rss(cpr::Url link);
+
+            /// parse ttl tag and sets to value, if value outside of range 0-60, default
+            int parse_ttl(const char* ttl_str);
+
             /// parse xml and extract wanted information into articles
             std::vector<news::Article> parse_rss(const cpr::Response& rss_res);
+
             /// crawl through at retrieve the actual article itself from link
-            std::string crawl_article(std::string link);
+            // std::string crawl_article(std::string link);
     };
 } // namespace news
 
